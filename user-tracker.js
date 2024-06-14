e2626bdd4e37a202fcdadcebedcac348f63df4e9
DC* <des@riseup.net>
2024-06-14 01:54:23 +0100

    Update tracker code

--- user-tracker.js
+++ user-tracker.js
@@ -1,5 +1,5 @@
 // source: https://stackoverflow.com/a/2117523
-function uuidv4(): string {
+function uuidv4() {
   return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, c =>
       (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
   );
@@ -7,11 +7,11 @@ function uuidv4(): string {
 
 const clientId = document.currentScript.dataset.client || '';
 
-(function (clientId: string): void {
+(function (clientId) {
   const endpoint = 'http://localhost:8001/api/click'
   const localStorageKey = 'u'
 
-  let uuid : string | null = localStorage.getItem(localStorageKey);
+  let uuid = localStorage.getItem(localStorageKey);
   if (!uuid) {
     localStorage.setItem(localStorageKey, uuidv4())
   }
@@ -23,7 +23,7 @@ const clientId = document.currentScript.dataset.client || '';
       'X-Authorization': clientId,
     },
     body: JSON.stringify({
-      pageId: window.location.href,
+      pageUrl: window.location.href,
     }),
   });
 
