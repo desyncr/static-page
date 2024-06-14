11ae15afd269b43ec7dd0e26972a368d8498fd2b
DC* <des@riseup.net>
2024-06-14 16:10:19 +0100

    Compact code

--- user-tracker.js
+++ user-tracker.js
@@ -1,13 +1,11 @@
-// source: https://stackoverflow.com/a/2117523
-function uuidv4() {
-  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, c =>
-      (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
-  );
-}
-
-const clientId = document.currentScript.dataset.client || '';
-
 (function (clientId) {
+  // source: https://stackoverflow.com/a/2117523
+  function uuidv4() {
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, c =>
+        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    );
+  }
+
   const endpoint = 'http://localhost:8001/api/click'
   const localStorageKey = 'u'
 
@@ -28,4 +26,4 @@ const clientId = document.currentScript.dataset.client || '';
     }),
   });
 
-})(clientId);
+})(document.currentScript.dataset.client || '');
